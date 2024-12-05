ee4f1528ba3d07c23c2c262e3b0716575c0550e6
Danang Priyombodo <32449624+overboxbgrff@users.noreply.github.com>
2024-12-05 09:48:29 +0800

    Add files via upload

--- sample_db/kas.sql
+++ sample_db/kas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2024 at 05:39 AM
+-- Generation Time: Dec 05, 2024 at 02:48 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -65,7 +65,7 @@ CREATE TABLE `jenis_km` (
 --
 
 INSERT INTO `jenis_km` (`id_km`, `kode_km`, `desk_km`) VALUES
-(1, 'S.A', 'Saldo Awal'),
+(1, '1-S.A', 'Saldo Awal'),
 (3, 'B.U', 'Bantuan Umum'),
 (5, 'K.101', 'Kas'),
 (6, 'M', 'Modal'),
@@ -80,7 +80,7 @@ INSERT INTO `jenis_km` (`id_km`, `kode_km`, `desk_km`) VALUES
 
 CREATE TABLE `kas_keluar` (
   `id_kkk` varchar(128) NOT NULL,
-  `tanggal_kk` varchar(64) NOT NULL,
+  `tanggal_kk` date NOT NULL,
   `kode_kk` varchar(64) NOT NULL,
   `uraian_kk` varchar(1024) NOT NULL,
   `jumlah_kk` int(255) NOT NULL
@@ -91,7 +91,7 @@ CREATE TABLE `kas_keluar` (
 --
 
 INSERT INTO `kas_keluar` (`id_kkk`, `tanggal_kk`, `kode_kk`, `uraian_kk`, `jumlah_kk`) VALUES
-('KK-20241203-120423', '03/12/2024', 'B.A', 'aaa', 120000);
+('KK-20241203-130154', '2024-12-03', 'B.A', 'Beban Admin', 15000000);
 
 -- --------------------------------------------------------
 
@@ -101,7 +101,7 @@ INSERT INTO `kas_keluar` (`id_kkk`, `tanggal_kk`, `kode_kk`, `uraian_kk`, `jumla
 
 CREATE TABLE `kas_masuk` (
   `id_kkm` varchar(128) NOT NULL,
-  `tanggal_km` varchar(64) NOT NULL,
+  `tanggal_km` date NOT NULL,
   `kode_km` varchar(64) NOT NULL,
   `uraian_km` varchar(1024) NOT NULL,
   `jumlah_km` int(255) NOT NULL
@@ -112,9 +112,24 @@ CREATE TABLE `kas_masuk` (
 --
 
 INSERT INTO `kas_masuk` (`id_kkm`, `tanggal_km`, `kode_km`, `uraian_km`, `jumlah_km`) VALUES
-('KM-20241203-121913', '2024-12-03', 'S.A', 'Saldo Awal Bulan', 200000000),
+('KM-20241203-121913', '2024-12-03', '1-S.A', 'Saldo Awal Bulan', 200000000),
 ('KM-20241203-122244', '2024-10-03', 'S.A', 'Saldo Awal Bulan Oktober', 250000000),
 ('KM-20241203-122318', '2024-12-04', 'B.U', 'Bantuan Pemda', 150000000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lapor`
+--
+
+CREATE TABLE `lapor` (
+  `id_lapor` varchar(128) NOT NULL,
+  `tanggal_lapor` date NOT NULL,
+  `kode_lapor` varchar(64) NOT NULL,
+  `uraian_lapor` varchar(1024) NOT NULL,
+  `pemasukan` int(255) NOT NULL,
+  `pengeluaran` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
